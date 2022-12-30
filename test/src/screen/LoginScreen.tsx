9f1c28b1191bbe0349f28c87159dc1062908e991
avishka05 <88711826+avishka05@users.noreply.github.com>
2022-12-30 15:46:02 +0530

    complete validation

--- test/src/screen/LoginScreen.tsx
+++ test/src/screen/LoginScreen.tsx
@@ -1,31 +1,36 @@
-import React ,{useState,SyntheticEvent} from 'react'
+import React  from 'react'
 import {Form,Button,Container,Row,Col } from 'react-bootstrap';
-import FormContainer from '../components/FormContainer';
-import { useNavigate  } from "react-router-dom";
+//import { useNavigate  } from "react-router-dom";
+import { useFormik } from 'formik';
+import {basicLoginvalidation} from '../Schemas/index'
+
+
 
 const LoginScreen = () => {
-  const navigate = useNavigate()
-  const[email,setemail]=useState('');
-  const[password,setpassword]=useState('');
+  //const navigate = useNavigate()
 
-  const SumbitHadler =(e:SyntheticEvent)=>{
-  e.preventDefault();
-  let data={email,password}
- // interact with backend using fetch
-    //  fetch('https://dummyjson.com/auth/login',{
-    //   method:'POST',
-    //   headers:{'Content-Type' :' application/json '},
-    //   credentials:'include',
-    //   body:JSON.stringify({
-    //     username: email,
-    //     password: password,
-        
-    //   })
 
-    // })
-    console.log("login data",data);
-     navigate("/");
-  }
+  const {values,errors,touched,handleBlur,handleSubmit,handleChange} = useFormik({
+    initialValues: {
+
+      email: '',
+      password: '',
+ 
+    },
+    validationSchema:basicLoginvalidation,
+    onSubmit: values => {
+    //  alert(JSON.stringify(values, null, 2));
+      console.log("Login details",values);
+      // navigate("/login")
+      
+    },
+  
+  });console.log('Errors found',errors);
+
+
+
+
+
   return (
     <div>
       <Container className='py-3'>
@@ -33,21 +38,26 @@ const LoginScreen = () => {
         <Col className="card-header py-3" >
         <h1 className="m-2 card-title">Login</h1>
          {/* <FormContainer> */}
-      <Form className="card-body" onSubmit={SumbitHadler}  >
+      <Form className="card-body" onSubmit={handleSubmit}  >
       
       <Form.Group className="my-3" controlId="email" >
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter your email" 
-        value={email}
-        onChange={e =>setemail(e.target.value)}/>
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.email  && touched.email ? "input-error" :""} />
+      {errors.email && touched.email &&<p className='error'>{errors.email}</p> }
         
       </Form.Group>
-      We'll never share your email with anyone else.
       <Form.Group className="my-3" controlId="password">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" 
-        value={password}
-        onChange={e =>setpassword(e.target.value)}/>
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.password && touched.password  ? "input-error" :""} />
+     {errors.password && touched.password &&<p className='error'>{errors.password}</p> }
       </Form.Group>
      
       <Button variant="dark" type="submit" >
--- test/src/screen/SignupScreen.tsx
+++ test/src/screen/SignupScreen.tsx
@@ -1,89 +1,118 @@
-import React ,{useState,SyntheticEvent} from 'react'
+import React from 'react'
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { useFormik } from 'formik';
+//import {useNavigate } from "react-router-dom";
+import {basicValidation} from '../Schemas/index'
 
-import { useNavigate  } from "react-router-dom";
+//import { navigate } from '@reach/router';
 
 
+const onSubmit= async (values: any,action: any) =>{
+
+  console.log('sumbited data is',values)
+  console.log('sumbited action is',action)
+  // await new Promise((resolve)=> setTimeout(resolve,1000));
+  // action.resetForm();
+
+
+}
 
 
 const SignupScreen = () => {
-  const navigate = useNavigate();
-  const[firstname,setfirstname]=useState('');
-  const[lastname,setlastname]=useState('');
-  const[email,setemail]=useState('');
-  const[password,setpassword]=useState(''); 
-  const[Cpassword,setCpassword]=useState('');
- 
+ // const navigate = useNavigate();
 
+  const {values,errors,touched,isSubmitting,handleBlur,handleSubmit,handleChange} = useFormik({
+    initialValues: {
+     firstname  :'',
+      lastname: '',
+      email: '',
+      password: '',
+      Cpassword: '',
+      phonenumber:'',
 
+    },
+    validationSchema:basicValidation,
+    onSubmit,
+   
+   
+  });
+  console.log('Errors found',errors);
 
-  //from sumbit method
-  const SumbitHadler =async(e:SyntheticEvent)=>{
-    e.preventDefault();
-
-    let data={firstname,lastname,email,password}
-    // interact with backend using fetch
-    // await fetch('url here',{
-    //   method:'POST',
-    //   headers:{'Content-Type' :' application/json '},
-    //   body:JSON.stringify({
-    //     firstname,lastname,email,password
-    //   })
-
-    // })
-    
-    navigate("/login")
-    console.log("data is",data)
-    console.log("Sumbitted")
-    }
 
   return (
     <div>
+
+
       <Container className='py-3'>
         <Row className='justify-content-md-center card shadow mb-4' >
           <Col className="card-header py-3" >
             <h1 className="m-2 card-title">Sign Up</h1>
             {/* <FormContainer> */}
-            <Form className="card-body" onSubmit={SumbitHadler}  >
+            <Form className="card-body" onSubmit={handleSubmit}  >
 
-            <Form.Group className="my-3" controlId="firstname" >
+              <Form.Group className="my-3" controlId="firstname" >
                 <Form.Label>First Name</Form.Label>
                 <Form.Control type="firstname" placeholder="Enter your firstname"
-                 value={firstname}
-                 onChange={e =>setfirstname(e.target.value)} />
-               
+                  value={values.firstname}
+                  onChange={handleChange}
+                  className={errors.firstname && touched.firstname  ? "input-error" :""} />
+                  {errors.firstname && touched.firstname &&<p className='error'>{errors.firstname}</p> }
+
+
               </Form.Group>
               <Form.Group className="my-3" controlId="lastname" >
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="lastname" placeholder="Enter your lastname" 
-                 value={lastname}
-                 onChange={e =>setlastname(e.target.value)}/>
-                <Form.Text className="text-muted">
-                </Form.Text>
+                <Form.Control type="lastname" placeholder="Enter your lastname"
+                  value={values.lastname}
+                  onChange={handleChange} 
+                  onBlur={handleBlur}
+                  className={errors.lastname && touched.lastname  ? "input-error" :""}/>
+                 {errors.lastname && touched.lastname &&<p className='error'>{errors.lastname}</p> }
+               
               </Form.Group>
+              <Form.Group className="my-3" controlId="phonenumber">
+                <Form.Label> Phone Number</Form.Label>
+                <Form.Control type="phonenumber" placeholder="Enter Your Phone Number"
+                  value={values.phonenumber}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.phonenumber && touched.phonenumber  ? "input-error" :""} />
+           {errors.phonenumber && touched.phonenumber &&<p className='error'>{errors.phonenumber}</p> }
+              </Form.Group>
+
+
               <Form.Group className="my-3" controlId="email" >
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter your email"
-                 value={email}
-                 onChange={e =>setemail(e.target.value)} />
-                <Form.Text className="text-muted">
-                </Form.Text>
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.email && touched.email  ? "input-error" :""} />
+               {errors.email && touched.email &&<p className='error'>{errors.email}</p> }
+              
               </Form.Group>
 
               <Form.Group className="my-3" controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" 
-                 value={password}
-                 onChange={e =>setpassword(e.target.value)}/>
+                <Form.Control type="password" placeholder="Password"
+                  value={values.password}
+                  onChange={handleChange} 
+                  onBlur={handleBlur}
+                  className={errors.password  && touched.password ? "input-error" :""}/>
+                {errors.password && touched.password &&<p className='error'>{errors.password}</p> }
               </Form.Group>
               <Form.Group className="my-3" controlId="Cpassword">
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control type="password" placeholder="Confirm Password"
-                   value={Cpassword}
-                   onChange={e =>setCpassword(e.target.value)}/> 
+                  value={values.Cpassword}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={errors.Cpassword && touched.Cpassword  ? "input-error" :""} />
+           {errors.Cpassword && touched.Cpassword &&<p className='error'>{errors.Cpassword}</p> }
               </Form.Group>
 
-              <Button variant="dark"className="my-3" type="submit" >
+
+              <Button variant="dark" disabled={isSubmitting} className="my-3" type="submit" >
                 Submit
               </Button>
             </Form>
@@ -91,6 +120,7 @@ const SignupScreen = () => {
           </Col>
         </Row>
       </Container>
+
     </div>
   )
 }
